0b1bce3d3fc1b9667c0ed7da34fd8810e5bf2295
AlexLaur <laurette.alexandre@gmail.com>
2021-02-28 23:58:02 +0100

    Docker added

--- src/server.py
+++ src/server.py
@@ -86,4 +86,5 @@ def create_project():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    # use host="0.0.0.0" for docker
+    app.run(debug=True, use_reloader=True, host="0.0.0.0")
