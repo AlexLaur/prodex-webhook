49f2b630749171a97e69e5041dfdfbc97e20558f
AlexLaur <laurette.alexandre@gmail.com>
2021-03-04 23:16:53 +0100

    fiw os.environ

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -34,10 +34,10 @@ __all__ = []  # Used to store all tasks which have been found
 celery_app = Celery(__name__)
 
 celery_app.conf.broker_url = os.environ.get(
-    "CELERY_BROKER_URL", "redis://localhost:6379"
+    "CELERY_BROKER", "redis://localhost:6379"
 )
 celery_app.conf.result_backend = os.environ.get(
-    "CELERY_RESULT_BACKEND", "redis://localhost:6379"
+    "CELERY_BACKEND", "redis://localhost:6379"
 )
 
 # Debub task
